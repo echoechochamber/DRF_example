18e30f07b4600caef52745f1a3e0e12da6f53950
A.K. Williams <akwillia2@gmail.com>
2019-09-29 22:50:26 -0400

    [refactor] serializers to use ModelSerializer class

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,20 +1,8 @@
 from rest_framework import serializers
 from todos.models import Todo
 
-class TodoSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    description = serializers.CharField(required=True, max_length=200)
-    note = serializers.CharField(required=False)
-
-    def create(self, validated_data):
-        """
-        create and return a todo instance based on given data
-        """
-        return Todo.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        # if the validated_data has a null field, return the current value
-        instance.description = validated_data.get('description', instance.description)
-        instance.note = validated_data.get('node', instance.note)
-        instance.save()
-        return instance
+class TodoSerializer(serializers.ModelSerializer):
+    # using the ModelSerializer class is a syntactic sugar for the basick create and update methods
+    class Meta:
+        model = Todo
+        fields = ['id', 'description', 'note']
